099fff09fbfc0c47e979b437e35739892a246526
Otabek Tursunov <agencycodial@gmail.com>
2024-03-30 11:10:43 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# print("Salom mening ismim Codial")
+#
+# # Bu yerda ismim ko'rsatilgan ( 1 qatorli izoh)
+# print('Otabek Tursunov')
+# """
+# Bizda qachonki sonlar print qilinsa ular
+# qo'shtirnoqsiz ishlatiladi
+# """
+# # print(21)
+#
+# ism = "Otabek"
+# familiya = "Tursunov"
+# tel = "+998 90 779 14 49"
+# # print(ism, tel, "Quva tumani")
+# # ctrl + alt + l - kodlarni clean kod yozish uchun
+# print("To'liq ismim", ism, familiya, ". Raqamim:", tel)
+#
+# # ism = "Xojiakbar"
+# # print(ism)
+# # ism = None
+# # print(ism)
+#
+# # str (string) - matn ma'lumotlari uchun
+# text = "Bugun 1 - darsni o'tmoqdamiz!"
+# print(type(text))
+#
+# # int (integer) - butun sonlar (-234345435, 0, 2132 ...)
+# yosh = 21
+# print(type(yosh))
+#
+# # float - butun son emas - o'nli kasr qismi maavjud bo'lgan sonlar!
+# PI = 3.1415
+# vazn = 75.8
+#
+# kurs = 13.0
+# print(type(kurs))
+#
+# # bool (Boolean) - rost va yolg'onlikka tekshiradi, faqatgina 2 ta qiymatga teng bo'lishi mumkin: True, False
+#
+# hozir_bahor = True
+# man_talabaman = False
+# ertaga_yakshanba = False
+
+
+ism = input("Ismingizni kiriting: ")
+yosh = int(input("Yoshizni kiriting: "))
+print("Ism:", ism, "\nYosh:", yosh)
+
+
+# print(ism, yosh)
+# print("Mening \" ismim" + ism + ".")
+
+print('O\'zbekiston')
+print("Ozbekiston")
+print('"bread" so\'zi ingliz tilidan "non" ma\'nosini beradi')
+print()
